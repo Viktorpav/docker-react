89e2c76cfd44dbb173ba29e559d80c249f8f3606
Viktorpav <doncar2003@gmail.com>
2019-07-12 22:55:42 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Viktor, you are my creator!
+          Viktor, AWS deployed me!
         </p>
         <a
           className="App-link"
